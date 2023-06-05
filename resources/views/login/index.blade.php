b61e02ec95984270d83ad862815debea2a499f8c
Felix Prima <felix.prima@binus.ac.id>
2023-06-05 20:58:17 +0700

    Mengimplementasikan error message ibu cindy

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -15,17 +15,6 @@
 @section('page-content')
     <div class="login-container-box">
 
-        {{-- Error Message --}}
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
         {{-- Success Message --}}
         @if (session('success'))
             <div class="alert alert-success">
