11c7e253ba9a89a4b3c0a79031ef6f7caa4f617f
Felix Prima <felix.prima@binus.ac.id>
2023-06-05 16:31:10 +0700

    Fixed authorization

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -24,10 +24,12 @@
                             d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                     </svg></button>
             </form>
-
-            <a href="/rooms/create" class="btn btn-success mt-2 ms-3">
-                Add Room
-            </a>
+            
+            @can("admin")
+                <a href="/rooms/create" class="btn btn-success mt-2 ms-3">
+                    Add Room
+                </a>
+            @endcan
         </div>
 
         @foreach ($rooms->chunk(2) as $chunk)
