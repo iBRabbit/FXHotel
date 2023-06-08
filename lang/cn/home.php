733c70bafeefce64cf60bf2537e750a36e6b3d55
Yulia Stefani <yuliastefanii@gmail.com>
2023-06-08 20:30:34 +0700

    fix lang

--- lang/cn/home.php
+++ lang/cn/home.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'banner_text' => 'Relaksasi yang terbaik',
-    'specialities' => 'Fasilitas Kami',
-    'delicacy' => 'Kelezatan Kami'
+    'banner_text' => '最好的放松方式',
+    'facilities' => '我们的特色',
+    'delicacy' => '我们的佳肴',
 ];
